e93cc6350f1a7856f6468f83e11c39fbd891fbb7
Rahul DMello <rvdmello96@gmail.com>
2017-05-10 05:00:02 +0530

    basic model without much server capabilities

--- index.js
+++ index.js
@@ -4,4 +4,30 @@
  * and open the template in the editor.
  */
 
+var http = require('http');
+var fs = require('fs');
+var app = http.createServer(function(request, response) {
+        response.writeHead(200, {"Context-Type": "text/html"});
+        fs.createReadStream("./home.html").pipe(response);
+});
 
+var io = require('socket.io')(app);
+
+io.on('connection', onRequest);
+
+function onRequest(socket) {
+    socket.on('RecieveNewQuestion', function(msg){
+        if (!(msg == "" || msg == null)) {
+            socket.emit('AttachNewQuestion', msg);
+            // send to nearby people so they can reply
+        }
+    });
+};
+
+
+io.on('disconnect', function(socket){
+    console.log('disconnect');
+});
+
+app.listen(8080);
+console.log("server is running...");
