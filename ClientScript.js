27ca1cf893d36b65857862dd8fb00f730a04809a
Rahul DMello <rvdmello96@gmail.com>
2017-06-08 15:47:26 +0530

    added comments and little formating

--- ClientScript.js
+++ ClientScript.js
@@ -4,12 +4,16 @@
  * and open the template in the editor.
  */
 
-
+// init socket
 var socket = io('http://localhost:8080');  // for localhost
 // var socket = io('https://calm-crag-26465.herokuapp.com/');  for server deploy
 
+// used to process enter submitting in the input field txtInput
+// txtInput takes the input for current question
 function isEnterKey(event) {
-    var check = validate();
+    var check = validate();  // validate to see if the question is blank or not
+    // keycode 13 is the enter key
+    // TODO show some sort of message or make the field red for some secs if the text is blank and user pressed enter
     if (event.keyCode === 13) {
         if (check) {
             document.getElementById("boxInput").class = "form-group";
@@ -20,40 +24,53 @@ function isEnterKey(event) {
     }
 }
 
-
+// checks whether the question is blank or not
+// true = not blank
 function validate() {
     var str = document.getElementById("txtInput").value;
     document.getElementById("submit").disabled = (str === "" || str == null);
     return !(str === "");
 }
 
+// emits an event to set current question and perform further processing on the server side
 function updateQuestion() {
     socket.emit('RecieveNewQuestion', document.getElementById('txtInput').value);
     document.getElementById('txtInput').value = "";
     validate();
 }
 
+// event responds back with the current question after validating it 
+// so we can set it as out current question in the navbar
 socket.on('AttachNewQuestion', function (msg) {
     document.getElementById('currQues').innerHTML = " Current Question: " + msg;
 });
 
+// TODO refactor this event
+// msg contains a list of questions and socketIDs of other clients within 2km radius
 socket.on('AddNewQuestionToList', function (msg) {
-    document.getElementById('questions').innerHTML = '';
+    var tablehtml = '<table class="table table-hover"><tbody>';  // set each column in a new row of a table. looks better this way imo
     Object.keys(msg).forEach(function (key) {
-        document.getElementById('questions').innerHTML += '<br/>' + msg[key];
+        tablehtml += '<tr><td>🠶 ' + msg[key]+ '</td></tr>';
     });
+    tablehtml += '</tbody></table>';
+    document.getElementById('questions').innerHTML = tablehtml;
 });
 
+
+// called when page loads and then every 10 secs for update on client's position
 function updatePosition() {
     updatePositionHelper(positionUpdateSuccessCallback, function () {
         console.log('Cannot get location');
     });
 }
 
+// success callback for updatePositionHelper
 function positionUpdateSuccessCallback(coords) {
+    // emit client's coordinated to the server
     socket.emit('UpdatePosition', {coords: {'latitude': coords.latitude, 'longitude': coords.longitude}});
 }
 
+// helper function for updatePosition because that sorry little ass cant do shit by itself
 function updatePositionHelper(successCallback, errorCallback) {
     successCallback = successCallback || function () {
     };
@@ -67,18 +84,18 @@ function updatePositionHelper(successCallback, errorCallback) {
         // We have a real geolocation service.
         try {
             function handleSuccess(position) {
-                successCallback(position.coords);
+                successCallback(position.coords);  // YAAYYYYY!!
             }
 
             geolocation.watchPosition(handleSuccess, errorCallback, {
-                enableHighAccuracy: true,
-                maximumAge: 5000 // 5 sec.
+                enableHighAccuracy: true,  // high accuracy because idk how but this fixes its problems with mobiles
+                maximumAge: 5000  // 5 sec.
             });
         } catch (err) {
-            errorCallback();
+            errorCallback();  // NOOOOOO ;_;
         }
     } else {
-        errorCallback();
+        errorCallback();  // NOOOOOO T_T
     }
-    setTimeout(updatePosition, 10000); // 10 secs
+    setTimeout(updatePosition, 10000);  // 10 secs
 }
--- index.js
+++ index.js
@@ -5,27 +5,43 @@
  */
 
 var http = require('http');
+// file system ?
 var fs = require('fs');
+// mongoclient to connect with db
 var MongoClient = require('mongodb').MongoClient;
+// initialized when MongoClient is connected to the mongodb
 var db;
+// url to the database
 var url = "mongodb://localhost:27017/userinfo";  // for localhost
 // var url = "mongodb://calm-crag-26465.herokuapp.com/userinfo";  for server deploy
+
+// when someone requests the root '/' page
 var app = http.createServer(function (request, response) {
     response.writeHead(200, {"Context-Type": "text/html"});
     fs.createReadStream("./home.html").pipe(response);  // returns the home page
 });
 
+// socket.io object to communicate with client
 var io = require('socket.io')(app);
 
+// on connection event use onRequest funtion
 io.on('connection', onRequest);
 
+// yeah this function it takes the client's socket as argument
+// apparantly this obj is a copy and they cant share self made properties
 function onRequest(socket) {
 
-    // TODO populate questions in this list
+    // self made property
+    // its populated with power of friendship and questions of your neighbours and their ids
     socket.questions = {};
 
+    // just log out the id for sanity check
     console.log("socket.id: " + socket.id);
+    
+    // init this guy or girl in the db using the associated socket.id
     db.collection("users").insertOne({socketID: socket.id});
+    
+    // if this socket sends in a question to be accepted by the all mighty it gets validated, processed and passed on here
     socket.on('RecieveNewQuestion', function (msg) {
         if (!(msg == "" || msg == null)) {
 
@@ -36,31 +52,42 @@ function onRequest(socket) {
                 if (err)
                     throw err;
                 // success
-                socket.emit('AttachNewQuestion', msg);
-                updateUsersQuestionList(socket, io.sockets.sockets);
+                socket.emit('AttachNewQuestion', msg); // he may set this question as his current question
+                updateUsersQuestionList(socket, io.sockets.sockets); // gotta update question list for everyone now that we have one more question in the market
             });
         }
     });
 
+    // aye aye i will gladly accept your new position and have my way with it muahahahah!
     socket.on('UpdatePosition', function (position) {
+        // update position for this socket in the database
         db.collection("users").update({socketID: socket.id}, {$set: {coords: position.coords}}, function (err, obj) {
             if (err)
                 throw err;
+            // if successful update question list cuz he may have travelled super quick like a speedster and maybe now everything's changed including the poeple around him and their questions
             updateUsersQuestionList(socket, io.sockets.sockets);
         });
     });
 
+    // NOOOO DONT LEAVE US PLEASE NOOOOO 
+    // if you do though lemme clear your document from db and your id and question from other people's sockets 
+    // just giving them an update nothing to worry about 
+    // bye we may miss you
     socket.on('disconnect', function () {
         var query = {socketID: socket.id};
+        // remove associated doc from db
         db.collection("users").remove(query, function (err, obj) {
             if (err)
                 throw err;
+            // if successful remove his question from other sockets
             Object.keys(io.sockets.sockets).forEach(function (id) {
-                if (io.sockets.sockets[id].questions[socket.id])
+                if (io.sockets.sockets[id].questions[socket.id])  // if it exist
                     delete io.sockets.sockets[id].questions[socket.id];
             });
+            // sanity check for how many got deleted
             console.log(obj.result.n + " document(s) deleted");
         });
+        // sanity check for who got deleted
         console.log('disconnect: ' + socket.id);
     });
 }
@@ -68,26 +95,32 @@ function onRequest(socket) {
 
 // will populate the socket.questions property with questions of people within 2km radius
 function updateUsersQuestionList(socket, sockets) {
+    // fetch me that slugger
     db.collection("users").findOne({socketID: socket.id}, function (err, result) {
         if (err)
             throw err;
-        user = result;
-        var coords1 = user.coords;
+        user = result;  // useless but at this point i am too afraid to change anything
+        var coords1 = user.coords;  // where the hell is he ayy ?
         if (coords1) {
+            // found you muahahhaha!
+            // bring everyone here get me everyone you hear me EVERYONE EVERYONEEEEE! may i request them to be in an array pls. thanks
             db.collection("users").find().toArray(function (err, result) {
                 if (err)
                     throw err;
 
+                // loop throw everyone to update their questions
                 for (var i = 0, l = result.length; i < l; i++) {
                     var coords2 = result[i].coords;
+                    // check for where we have his coords and if he is nearby (2km)
                     if (coords2 && getDistanceFromLatLonInKm(coords1.latitude, coords1.longitude, coords2.latitude, coords2.longitude) < 2) {
-                        socket.questions[result[i].socketID] = result[i].curr_ques;
+                        socket.questions[result[i].socketID] = result[i].curr_ques;  // add if nearby
                     } else {
                         if (socket.questions[result[i].socketID])
-                            delete socket.questions[result[i].socketID];
+                            delete socket.questions[result[i].socketID];  // delete if he is no more nearby, because thats how we roll xD
                     }
                 }
-
+                
+                // aite i will let the client side know to update his list and we donezo woooohoooo!!!
                 socket.emit('AddNewQuestionToList', socket.questions);
 
             });
@@ -95,6 +128,8 @@ function updateUsersQuestionList(socket, sockets) {
     });
 }
 
+// courtesy of stackoverflow
+// yes get me that distance. the returned value is apparently in kms how convenient
 function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
     var R = 6371;  // Radius of the earth in km
     var dLat = deg2rad(lat2 - lat1);  // deg2rad below
@@ -109,16 +144,24 @@ function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
     return d;
 }
 
+// the above function needs radian values and not degrees >.>
 function deg2rad(deg) {
     return deg * (Math.PI / 180);
 }
 
+// client connects with the db using the specified url (loop above)
 MongoClient.connect(url, function (err, database) {
     if (err)
         throw err;
-    db = database;
+    // if successful
+    db = database;  // like promised
+    // again, sanity check
     console.log('database connected!');
+    
+    // specify port
     // app.listen((process.env.PORT || 8080));  for server deploy
     app.listen(8080);  // for localhost
+    
+    // we made it. it, its so beautiful (*^▽^*)
     console.log("server is running...");
 });
